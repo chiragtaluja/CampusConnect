e7ad2a4ab519b8cd695cb7d1654233f82abac981
Bhavya <rohitno1111@gmail.com>
2025-08-18 21:16:43 +0530

    Added footer

--- EventManagementSystem/urls.py
+++ EventManagementSystem/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls), 
     path("", include("base.urls")),
-    path("__reload__/", include("django_browser_reload.urls")),
+    
     
     ]
 
